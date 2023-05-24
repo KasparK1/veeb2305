7ca142618285405cd5c7feab0087f9e622291741
KasparK1 <130385209+KasparK1@users.noreply.github.com>
2023-05-24 14:23:24 +0300

    kommit 03

--- loeng02/server/index.js
+++ loeng02/server/index.js
@@ -3,14 +3,46 @@ const http = require("http");
 const host = 'localhost';
 const port = 8000;
 
+let matk1 = {
+  title: 'Matk ümber Ülemiste',
+  imageUrl: 'https://lh5.googleusercontent.com/p/AF1QipORwhgR234cnj9UsRg1wZAArFXZUHPKUui24bc7=w300-h200-k-no',
+  description: 'Jalga panna kõrge säärega saapad!',
+  startTime: new Date(2023, 4, 27, 10, 0, 0),
+  lat: 59.3763953,
+  long: 24.7488814,
+  participants: ['Mihkel']
+}
+
+let matk2 = {
+  title: 'Merematk ümber Suurupi poolsaare',
+  imageUrl: 'https://lh5.googleusercontent.com/p/AF1QipN7PyVsqrmPCDXjPRRaNJ5XyPmTkTR3z9baUoiZ=w300-h200-k-no',
+  description: 'Alustame Lohusalust, lõpetame Kakumäel',
+  startTime: new Date(2023, 5, 24, 19, 30, 0),
+  lat: 59.4038059,
+  long: 24.2013333,
+  participants: ['Mihkel']
+}
+
+let matk3 = {
+    title: 'Kanuumatk Ahja jõel',
+    imageUrl: 'https://static.visitestonia.com/images/3643198/1600_900_false_false_483655a8d4881586159051aec359b361.jpg',
+    description: 'Alustame Lohusalust, lõpetame Kakumäel',
+    startTime: new Date(2023, 5, 24, 19, 30, 0),
+    lat: 59.4038059,
+    long: 24.2013333,
+    participants: ['Mihkel']
+  }
+
+let matkad = [matk1, matk2, matk3]
+
 const requestListener = function (req, res) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader("Content-Type", "application/json");
-    res.writeHead(200);
-    res.end(`{"message": "This is a JSON response"}`);
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Content-Type', 'application/json');
+  res.writeHead(200);
+  res.end(JSON.stringify(matkad));
 };
 
 const server = http.createServer(requestListener);
-server.listen(port, host, () => {
+server.listen(port, host, function () {
     console.log(`Server is running on http://${host}:${port}`);
 });
